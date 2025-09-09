278dbbdb82913a27c97c889d1fd73578fc2b5f60
lucaslegor <134739969+lucaslegor@users.noreply.github.com>
2025-09-09 16:10:12 -0300

    next.config.mjs

--- cifras_significativas/next.config.mjs
+++ cifras_significativas/next.config.mjs
@@ -10,5 +10,15 @@ const nextConfig = {
     unoptimized: true,
   },
 }
+const isProd = process.env.NODE_ENV === 'production'
+const repoName = 'ANM---GANG-OF-FOUR---GRUPO-06'
+
+module.exports = {
+  output: 'export',
+  images: { unoptimized: true },
+  basePath: isProd ? `/${repoName}` : '',
+  assetPrefix: isProd ? `/${repoName}/` : '',
+  trailingSlash: true,
+}
 
 export default nextConfig
